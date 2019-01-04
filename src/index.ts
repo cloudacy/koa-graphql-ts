e9fae27ccb930d671ef2595af7b4b7a8f88b0d68
Patrick <patrick.kuen11@gmail.com>
2019-01-04 11:11:52 +0100

    Removed some console logs

--- src/index.ts
+++ src/index.ts
@@ -33,21 +33,16 @@ export const handleErrors = function(result: ExecutionResultDataDefault, formatE
 
 export const graphQLServer = function(options: GraphQLServerOptions) {
   return async function(ctx: ExtendedParameterizedContext, next: () => Promise<void>) {
-    console.log('server')
     if (options.graphiql) {
       ctx.type = 'text/html'
       ctx.status = 200
       ctx.body = renderGraphiQL({})
     } else {
       try {
-        console.log('gql request')
         const result = await graphql(options.schema, ctx.request.body ? ctx.request.body.query : null, null, ctx, ctx.request.body ? ctx.request.body.variables || null : null)
-        console.log('res')
         ctx.body = result
-        console.log(result, ctx.body)
         handleErrors(result, options.formatError)
       } catch (error) {
-        console.error(error)
         ctx.status = error.status || 500
         handleErrors({errors: [error]}, options.formatError)
       }
