6c4f685d0556b75aeafbf3e106ac7bcedfe39dd9
Patrick Kuen <patrick.kuen11@gmail.com>
2019-12-17 14:45:23 +0100

    Remove GraphiQL to use new graphql playground koa middleware package.

---
+++ README.md
@@ -0,0 +1,4 @@
+# koa-graphql-ts
+
+This Middleware implements the graphql server for koa.
+To use the new GraphQL Playground use the [graphql-playground-middleware-koa](https://github.com/prisma-labs/graphql-playground/tree/master/packages/graphql-playground-middleware-koa) package.
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { ParameterizedContext, Request } from "koa";
 import { GraphQLSchema, graphql } from "graphql";
-import { renderGraphiQL } from "./graphiql";
 import { ExecutionResultDataDefault } from "graphql/execution/execute";
 
 export type ErrorFunction = (error: any, returnNull?: boolean | undefined) => any
 
 export interface GraphQLServerOptions {
   schema: GraphQLSchema
-  graphiql?: boolean
   rootValue?: any
   ctx?: any
   pretty?: boolean
@@ -32,20 +30,14 @@ export const handleErrors = function(result: ExecutionResultDataDefault, formatE
 }
 
 export const graphQLServer = function(options: GraphQLServerOptions) {
-  return async function(ctx: ExtendedParameterizedContext, next: () => Promise<void>) {
-    if (options.graphiql) {
-      ctx.type = 'text/html'
-      ctx.status = 200
-      ctx.body = renderGraphiQL({})
-    } else {
-      try {
-        const result = await graphql(options.schema, ctx.request.body ? ctx.request.body.query : null, null, ctx, ctx.request.body ? ctx.request.body.variables || null : null)
-        ctx.body = result
-        handleErrors(result, options.formatError)
-      } catch (error) {
-        ctx.status = error.status || 500
-        handleErrors({errors: [error]}, options.formatError)
-      }
+  return async function(ctx: ExtendedParameterizedContext) {
+    try {
+      const result = await graphql(options.schema, ctx.request.body ? ctx.request.body.query : null, null, ctx, ctx.request.body ? ctx.request.body.variables || null : null)
+      ctx.body = result
+      handleErrors(result, options.formatError)
+    } catch (error) {
+      ctx.status = error.status || 500
+      handleErrors({errors: [error]}, options.formatError)
     }
   }
 }
